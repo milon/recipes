ccccabea3c4c1183378565348ce23cfc90f3459b
Nuruzzaman Milon <contact@milon.im>
2020-04-04 00:24:19 +0200

    prepare about and contact page

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,8 +1,2 @@
 // Bootstrap core JavaScript
 import 'bootstrap';
-
-// Highlight.js syntax highlighting
-let hljs = require('highlight.js');
-
-// Initialise Highlight.js on page load
-hljs.initHighlightingOnLoad();

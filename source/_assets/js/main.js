ad7548fa1850d87308283e67d07ac161f9ca7803
Nuruzzaman Milon <contact@milon.im>
2020-04-04 16:29:33 +0200

    added dynamic changing background

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,2 +1,10 @@
 // Bootstrap core JavaScript
 import 'bootstrap';
+
+// dynamically change background in every 20 seconds
+setInterval(function() {
+    let random = Math.floor(Math.random() * 10) + 1;
+    let imageUrl = `/assets/images/backgrounds/bg-${random}.jpg`;
+    let banner = document.querySelector('#banner');
+    banner.style.backgroundImage = `url(${imageUrl})`;
+}, 20*1000);
--- source/_layouts/page.blade.php
+++ source/_layouts/page.blade.php
@@ -5,7 +5,7 @@
     @include('_partials.navigation')
 
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url({{ $page->image ? $page->image : $page->randomBackground() }})">
+    <header class="masthead" id="banner" style="background-image: url({{ $page->image ? $page->image : $page->randomBackground() }})">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">

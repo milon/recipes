375b0cfb3cf63107083e26199d56ca8c1e61638e
Nuruzzaman Milon <contact@milon.im>
2020-12-29 11:47:12 +0100

    fix post navigation

--- source/_partials/post_pagination.blade.php
+++ source/_partials/post_pagination.blade.php
@@ -1,14 +1,14 @@
 <div class="clearfix">
     @if ($page->getPrevious())
-        <a href="{{ $page->getPrevious()->getPath() }}" class="float-left">
+        <a href="{{ $page->getPrevious()->getPath('web') }}" class="float-left">
             <i class="fas fa-angle-left"></i>
             {{ $page->getPrevious()->title }}
         </a>
     @endif
     @if ($page->getNext())
-        <a href="{{ $page->getNext()->getPath() }}" class="float-right">
+        <a href="{{ $page->getNext()->getPath('web') }}" class="float-right">
             {{ $page->getNext()->title }}
-            <i class="fas fa-angle-right"></i> 
+            <i class="fas fa-angle-right"></i>
         </a>
     @endif
 </div>
